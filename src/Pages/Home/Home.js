c1e80e8a1619c6b6576b6b8668b8212e01d00d8f
Maryana Dey <Dey.maryana1989@gmail.com>
2023-04-28 19:21:15 +0200

    add photo img and catogories-list

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,23 @@
 import { useState } from 'react';
 import './Home.css';
 
+import chocolateCandy from './../../assets/images/Rectangle 48.jpg';
+
+
 const products = [
-  { id: 1, name: 'Шоколадні цукерки', category: 'chocolate' },
-  { id: 2, name: 'Фруктові цукерки', category: 'fruit' },
-  { id: 3, name: 'Карамельні цукерки', category: 'caramel' },
-  { id: 4, name: 'Шоколадні цукерки 2', category: 'chocolate' },
-  { id: 5, name: 'Фруктові цукерки 2', category: 'fruit' },
-  { id: 6, name: 'Карамельні цукерки 2', category: 'caramel' },
+  { id: 1, name: 'Шоколадні цукерки', category: 'chocolate', image: chocolateCandy },
+  { id: 2, name: 'Фруктові цукерки', category: 'fruit', image: chocolateCandy },
+  { id: 3, name: 'Карамельні цукерки', category: 'caramel', image: chocolateCandy },
+  { id: 4, name: 'Шоколадні цукерки 2', category: 'chocolate', image: chocolateCandy },
+  { id: 5, name: 'Фруктові цукерки 2', category: 'fruit', image: chocolateCandy },
+  { id: 6, name: 'Карамельні цукерки 2', category: 'caramel', image: chocolateCandy },
+
+  { id: 7, name: 'Шоколадні цукерки3', category: 'chocolate', image: chocolateCandy },
+  { id: 8, name: 'Фруктові цукерки3', category: 'fruit', image: chocolateCandy },
+  { id: 9, name: 'Карамельні цукерки3', category: 'caramel', image: chocolateCandy },
+  { id: 10, name: 'Шоколадні цукерки 4', category: 'chocolate', image: chocolateCandy },
+  { id: 11, name: 'Фруктові цукерки 4', category: 'fruit', image: chocolateCandy },
+  { id: 12, name: 'Карамельні цукерки 4', category: 'caramel', image: chocolateCandy },
 ];
 
 
@@ -18,29 +28,8 @@ function Home() {
     setCategory(category);
   };
 
-  const filteredProducts = category
-  ? products.filter((product) => product.category === category)
-  : products;
+  const filteredProducts = category ? products.filter((product) => product.category === category) : products;
 
-
-  
-//   const handleSortByCategory = (category) => {
-//   if (category === 'chocolate') {
-//     setCategory('chocolate');
-//   } else {
-//     setCategory('');
-//   }
-// };
-  
-  //   const handleSortByCategory = (category) => {
-  // if (category === 'chocolate') {
-  //   setCategory('chocolate');
-  // } else {
-  //   setCategory('');
-  // }
-  // };
-
-  // const filteredProducts = category ? products.filter((product) => product.category === category) : products;
 
   // Розбиваємо масив на ряди по 3 товара
   const productRows = filteredProducts.reduce((acc, product, index) => {
@@ -61,7 +50,7 @@ function Home() {
 
       <div className="categories">
         <h2>Категорії цукерок:</h2>
-        <ul>
+        <ul className="categories-list">
           <li>
             <button onClick={() => handleSortByCategory('chocolate')}>Шоколадні цукерки</button>
           </li>
@@ -79,6 +68,7 @@ function Home() {
           <div key={index} className="product-row">
             {row.map((product) => (
               <div key={product.id} className="product-card">
+                 <img src={product.image} alt={product.category} />
                 <h3>{product.name}</h3>
                 <p>Категорія: {product.category}</p>
               </div>
